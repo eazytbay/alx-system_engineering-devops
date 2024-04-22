8578f999104a1675dfd2894f137ee9d7c6167b13
eazytbay <eazytbay@gmail.com>
2024-04-22 04:49:10 -0700

    3-dictionary_of_list_of_dictionaries.py updated

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,28 +7,16 @@ Implemented using recursion
 import json
 import requests
 import sys
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/users"
-
-    resp = requests.get(url)
-    users = response.json()
-
-    dictionary = {}
-    for user in users:
-        user_id = user.get('id')
-        username = user.get('username')
-        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-        url = url + '/todos/'
-        resp = requests.get(url)
-        tsks = resp.json()
-        dictionary[user_id] = []
-        for tsk in tsks:
-            dictionary[user_id].append({
-                "task": tsk.get('title'),
-                "completed": tsk.get('completed'),
-                "username": username
-            })
-    with open('todo_all_employees.json', 'w') as file:
-        json.dump(dictionary, file)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            y.get("id"): [{
+                "task": x.get("title"),
+                "completed": x.get("completed"),
+                "username": y.get("username")
+            } for x in requests.get(url + "todos",
+                                    params={"userId": y.get("id")}).json()]
+            for y in users}, jsonfile)

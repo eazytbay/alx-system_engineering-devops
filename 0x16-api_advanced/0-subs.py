009858685212c8a9bbbb582df0dd688a5349c18e
eazytbay <eazytbay@gmail.com>
2024-05-08 02:50:54 +0000

    0-subs.py another fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,29 +4,11 @@ import requests
 import json
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers, or 0 if an error occurs.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0 
-
-    try:
-        # Using format method for URL
-        url = 'http://www.reddit.com/r/{}/{}.json'.format(subreddit, 'about')
-        headers = {'User-Agent': 'Python/requests:APIproject:v1.0.0 (by /u/aaorrico23)'}
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-
-        data = json.loads(response.text)
-        subscribers = data.get("data", {}).get("subscribers", 0)
-        return subscribers
-
-    except requests.exceptions.RequestException as e:
-        print("Error retrieving subscriber count for '%s': %s" % (subreddit, e))
-        return 0 
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+v1.0.0 (by /u/aaorrico23)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs

289d58fd34d9c76319e5a4149dfbca572feb0c9a
eazytbay <eazytbay@gmail.com>
2024-05-31 02:41:48 -0700

    1-top_ten.py updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
-"""This is a function that prints hot posts on a given Reddit subreddit."""
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
-def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
-    params = {
-        "limit": 10
-    }
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        response.raise_for_status()  # Raise an exception for unexpected status codes
-        results = response.json().get("data")
-        if results:
-            [print(c.get("data").get("title")) for c in results.get("children")]
-        else:
-            print("No posts found.")
-    except requests.exceptions.HTTPError as e:
-        if response.status_code == 403:
-            print("Error: You may not have permission to access this subreddit.")
-        else:
-            print("HTTP Error:", e)
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
 
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+
+
+def top_ten(subreddit):
+    '''Retrieves the title of the top ten posts from a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'top'
+    limit = 10
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        for post in res.json()['data']['children'][0:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
